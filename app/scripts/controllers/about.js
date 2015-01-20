efae22a8ceca0266dec458b8d04de5db3709a0f4
herereadthis <herereadthis@gmail.com>
2015-01-19 22:36:06 -0500

    formatting for whitespace

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,10 @@
  * Controller of the mousethiefApp
  */
 angular.module('mousethiefApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('AboutCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
