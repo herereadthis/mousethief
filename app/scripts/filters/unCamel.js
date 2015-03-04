2ee0e10b49709deb908fcb8ab57f9a68110170d7
herereadthis <herereadthis@gmail.com>
2015-03-04 16:26:49 -0500

    fix possible undefined replace

--- app/scripts/filters/unCamel.js
+++ app/scripts/filters/unCamel.js
@@ -7,6 +7,11 @@ unCamelFilter.filter('unCamel', function() {
     return function(input) {
         var titleString;
 
+        // if the filter is taking in nothing
+        if (_.isUndefined(input)) {
+            return;
+        }
+
         // split where capital letters exist to spaces
         titleString = input.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
         titleString = titleString.replace(/^./, function(string) {
